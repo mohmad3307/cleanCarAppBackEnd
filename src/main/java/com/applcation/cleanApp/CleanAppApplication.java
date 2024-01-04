d7c9ad8d367629e0885f1e9c3ce74de663b1b722
mohmad3308 <mohmadmajadly8@gmail.com>
2024-01-04 15:48:20 +0200

    edit main paths second

--- src/main/java/com/applcation/cleanApp/CleanAppApplication.java
+++ src/main/java/com/applcation/cleanApp/CleanAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CleanAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CleanAppApplication.class, args);
 	}
-
 }
